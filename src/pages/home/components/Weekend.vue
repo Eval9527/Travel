ace81eb272e81a907615d3cd4dd232b61019ac48
Pooc <pooc95@163.com>
2019-03-15 15:49:15 +0800

    add weekend

---
+++ src/pages/home/components/Weekend.vue
@@ -0,0 +1,74 @@
+<template>
+  <div>
+    <div class="title">周末去哪</div>
+    <ul>
+      <li class="item border-bottom" v-for="item of recommendList" :key="item.id">
+        <div class="item-img-paper">
+          <img class="item-img" :src="item.imgUrl">
+        </div>
+        <div class="item-info">
+          <p class="item-title">{{item.title}}</p>
+          <p class="item-desc">{{item.desc}}</p>
+        </div>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeWeekend',
+  data () {
+    return {
+      recommendList: [{
+        id: '001',
+        imgUrl: 'http://img1.qunarzz.com/sight/source/1811/15/66f14e0fd6fbb.jpg_r_640x214_5d69f21d.jpg',
+        title: '长隆野生动物世界',
+        desc: '长隆野生动物世界的心脏位置。'
+      }, {
+        id: '002',
+        imgUrl: 'http://img1.qunarzz.com/sight/source/1811/15/66f14e0fd6fbb.jpg_r_640x214_5d69f21d.jpg',
+        title: '长隆野生动物世界',
+        desc: '长隆野生动物世界的心脏位置。'
+      }, {
+        id: '003',
+        imgUrl: 'http://img1.qunarzz.com/sight/source/1811/15/66f14e0fd6fbb.jpg_r_640x214_5d69f21d.jpg',
+        title: '长隆野生动物世界',
+        desc: '长隆野生动物世界的心脏位置。'
+      }]
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+  @import "~styles/mixins.styl"
+  .title
+    margin-top: .2rem
+    line-height: .8rem
+    background: #eee
+    text-indent: .2rem
+  .item-img-paper
+    overflow: hidden
+    height: 0
+    padding-bottom: (214/640)*100%
+    .item-img
+      width: 100%
+  .item-info
+    padding: .1rem
+    .item-title
+      line-height: .54rem
+      font-size: .32rem
+      ellipsis()
+    .item-desc
+      line-height: .4rem
+      color: #ccc
+      ellipsis()
+    .item-button
+      line-height: .44rem
+      margin-top: .16rem
+      background: #ff9300
+      padding: 0 .2rem
+      border-radius: .06rem
+      color: #fff
+</style>
