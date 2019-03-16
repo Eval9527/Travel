e5b43f3d4c2d3f9a02ca5e6ebd2e40a9d8d5729b
Pooc <pooc95@163.com>
2019-03-16 17:38:28 +0800

    add city-search

---
+++ src/pages/city/components/Search.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="search">
+      <input class="input-search" type="text" placeholder="输入城市名或拼音">
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'CitySearch'
+}
+</script>
+
+<style lang="stylus" scoped>
+  @import "~styles/varibles.styl"
+  .search
+    height: .72rem
+    padding: 0 .1rem
+    background: $bgColor
+    .input-search
+      box-sizing: border-box  /*直接使用 padding 会导致右边溢出，需要使用 border-box 将内边距放入框中*/
+      width: 100%
+      height: .62rem
+      padding: 0 0.1rem
+      line-height: .62rem
+      text-align: center
+      border-radius: .06rem
+      color: #666
+</style>
