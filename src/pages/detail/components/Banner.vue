09b5765c62866789f95a6c0b9dd613b515ea4c32
Pooc <pooc95@163.com>
2019-03-21 15:32:25 +0800

    banner finish

---
+++ src/pages/detail/components/Banner.vue
@@ -0,0 +1,75 @@
+<template>
+  <div>
+    <div class="banner" @click="handleBannerClick">
+      <img  class="banner-img" src="//img1.qunarzz.com/sight/p0/1709/41/411f234d79457081a3.img.jpg_600x330_b5e86902.jpg">
+      <div class="banner-info">
+        <div class="banner-title">长隆野生动物世界(AAAAA景区)</div>
+        <div class="banner-number banner-icon"><span class="iconfont">&#xe650;</span>102</div>
+      </div>
+    </div>
+    <common-gallery
+      :imgs="imgs"
+      v-show="showGarrery"
+      @close="handleGalleryClose"
+    ></common-gallery>
+  </div>
+</template>
+
+<script>
+import CommonGallery from 'common/Gallery/Gallery'
+
+export default {
+  name: 'DetailBanner',
+  data () {
+    return {
+      showGarrery: false,
+      imgs: ['http://img1.qunarzz.com/sight/p0/1709/41/411f234d79457081a3.img.jpg_r_800x800_ea3a0585.jpg', 'http://img1.qunarzz.com/sight/p0/1709/41/411f234d79457081a3.img.jpg_r_800x800_ea3a0585.jpg']
+    }
+  },
+  methods: {
+    handleBannerClick () {
+      this.showGarrery = true
+    },
+    handleGalleryClose () {
+      this.showGarrery = false
+    }
+  },
+  components: {
+    CommonGallery
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+  .banner
+    position: relative
+    overflow: hidden
+    height:0
+    padding-bottom: (330/600)*100%
+    .banner-img
+      width: 100%
+    .banner-info
+      display: flex
+      position: absolute
+      left: 0
+      right: 0
+      bottom: 0
+      line-height: .6rem
+      color: #fff
+      //底部渐变效果
+      background-image: linear-gradient(top, rgba(0, 0, 0, 0), rgba(0, 0, 0, 0.8))
+      .banner-title
+        flex: 1
+        font-size: .32rem
+        padding: 0 .2rem
+      .banner-number
+        height: .32rem
+        line-height: .32rem
+        margin-top: .14rem
+        padding: 0 .4rem
+        border-radius: .2rem
+        background: rgba(0, 0, 0, .6)
+        font-size: .24rem
+        .banner-icon
+          font-size: .24rem
+</style>
